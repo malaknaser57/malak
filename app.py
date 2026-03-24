d817405d9594deb6d31cafe8afc87a3b1f9d4fa1
malaknaser57 <malaknaser355@gmail.com>
2026-03-24 02:50:32 +0200

    Add predict route for spam detection

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template('index.html')
+
+@app.route('/predict',methods=['POST']
+def predict():
+  email_text=request.form['email']
+
+  if 'free' in email_text.lower():
+    result = 'spam'
+  else:
+    result = 'Not Spam'
+  return result
 if __name__=='__main__':
   app.run()
