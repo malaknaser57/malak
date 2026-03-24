34db43b77126e3b4cb5544edf0c8197e4ffac5a7
malaknaser57 <malaknaser355@gmail.com>
2026-03-24 23:34:13 +0200

    Enhance email prediction functionality in app.py
    Refactor the Flask app to include model loading and prediction logic.

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import Flask, render_template,request
-app = Flask(__name__)
-@app.route('/')
-def home():
-  return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-  email_text=request.form['email']
-
-  if 'free' in email_text.lower():
-    result = 'spam'
-  else:
-    result = 'Not Spam'
-  return render_template('index.html',prediction=result)
-if __name__=='__main__':
-  app.run()
+from flask import Flask, render_template, request
 import pickle
 
-model = pickle.load(open("model.pkl","rb"))
+app = Flask(__name__)
+
+# تحميل الموديل
+model, vectorizer = pickle.load(open("model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email_text = request.form["email"]
+
+    # تحويل النص لارقام
+    text_vec = vectorizer.transform([email_text])
+
+    # التوقع
+    prediction = model.predict(text_vec)[0]
+
+    if prediction == 1:
+        result = "Spam Email 🚨"
+    else:
+        result = "Not Spam ✅"
+
+    return render_template("index.html", prediction_text=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
